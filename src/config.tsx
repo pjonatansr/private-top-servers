1e20a461ca044c33de35c79d8ed0a04726bede47
Pablo Jonatan <this.pablo@gmail.com>
2022-05-07 15:14:10 -0300

    refactor: changed logo const to banner

--- src/config.tsx
+++ src/config.tsx
@@ -1,5 +1,5 @@
 export const config = {
   GAME_NAME: 'Lineage 2',
   TOP_QUANTITY: 100,
-  LOGO: 'https://mmovicio.com.br/wp-content/uploads/2020/08/DK_aden_castle_poster.jpg',
+  BANNER: 'https://mmovicio.com.br/wp-content/uploads/2020/08/DK_aden_castle_poster.jpg',
 } as const;

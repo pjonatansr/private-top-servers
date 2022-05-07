2b10b8ce73a068061a15765772956ad9e9046391
Pablo Jonatan <this.pablo@gmail.com>
2022-05-07 15:11:11 -0300

    feat: added color mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { theme } from '../theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Head>
         <meta
           name="viewport"
